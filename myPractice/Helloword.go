2a8d8e098057fb71f168e2e14097dc85457362de
johnenes <johneneojo1@gmail.com>
2023-11-22 13:59:22 +0000

    more practice

--- myPractice/Helloword.go
+++ myPractice/Helloword.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 func main(){
+	// var astring string 
+	greetings := "Hello welcome ! ,"
+	 astring := "Halima Abdulkadir"
+	 sss3 :=" SSS3 class"
+	fmt.Println("Here is a string variable",greetings + astring + sss3)
 
-	fmt.Println("Hello World! Go")
+	var anIntger int =42;
+	fmt.Println("this an  Integer" , anIntger)
+
+	// fmt.Println("Hello World! Go")
+
+
+	 
 }
